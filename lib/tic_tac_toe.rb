017e10026599df8580422e172f3a2f139465d98e
Karen C <chenyanhuan66@163.com>
2018-07-12 08:20:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -136,14 +136,3 @@ def play(board)
 end
 
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.chomp
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index,'X')
-    display_board(board)
-  else
-    turn(board)
-  end
-end
